adc5106ded38fc2fb96dbbe16d87e11514968655
Cyril Allignol <cyril.allignol@enac.fr>
2015-04-15 18:23:06 +0200

    All shapes readable (not tested) + huge refactoring

--- shp.ml
+++ shp.ml
@@ -1,119 +1,188 @@
-type point = { x: float; y: float; z: float; m: float }
+let b2f = Int64.float_of_bits
+let b2i = Int32.to_int
 
-let print_point = fun p -> Printf.printf "x=%f y=%f z=%f m=%f\n" p.x p.y p.z p.m
+let xy = 2 and xym = 3 and xyzm = 4
+type point = float array
+type bbox = float array
 
-type bbox = { xmin: float; xmax: float; ymin: float; ymax: float;
-	      zmin: float; zmax: float; mmin: float; mmax: float }
-
-let bbox = fun xmin xmax ymin ymax zmin zmax mmin mmax ->
-  { xmin = Int64.float_of_bits xmin; ymin = Int64.float_of_bits ymin;
-    xmax = Int64.float_of_bits xmax; ymax = Int64.float_of_bits ymax;
-    zmin = Int64.float_of_bits zmin; zmax = Int64.float_of_bits zmax;
-    mmin = Int64.float_of_bits mmin; mmax = Int64.float_of_bits mmax }
-
-let bbox2D = fun xmin xmax ymin ymax -> bbox xmin xmax ymin ymax 0L 0L 0L 0L
-let bbox2DM = fun xmin xmax ymin ymax mmin mmax ->
-  bbox xmin xmax ymin ymax 0L 0L mmin mmax
-
-let print_bbox = fun b ->
-  Printf.printf "xmin = %f, xmax = %f\n" b.xmin b.xmax;
-  Printf.printf "ymin = %f, ymax = %f\n" b.ymin b.ymax;
-  Printf.printf "zmin = %f, zmax = %f\n" b.zmin b.zmax;
-  Printf.printf "mmin = %f, mmax = %f\n" b.mmin b.mmax
-
-type header = { length: int; version: int; shape_type: int; bbox: bbox }
-
-let print_header = fun h ->
-  Printf.printf "file length = %d\n%!" h.length;
-  Printf.printf "version = %d\n%!" h.version;
-  Printf.printf "type = %d\n%!" h.shape_type;
-  print_bbox h.bbox
-
-let header = fun bits ->
+let get_float = fun bits ->
   bitmatch bits with
-  | { code: 32 : bigendian, check (Int32.to_int code = 9994);
-      _: 5 * 32 : bitstring; (* 5 unused fields (32 bits each) *)
-      length: 32 : bigendian; version: 32 : littleendian;
-      shape_type: 32 : littleendian;
-      xmin: 64 : littleendian; ymin: 64 : littleendian;
-      xmax: 64 : littleendian; ymax: 64 : littleendian;
-      zmin: 64 : littleendian; zmax: 64 : littleendian;
-      mmin: 64 : littleendian; mmax: 64 : littleendian;
-      contents: -1 : bitstring
-    } ->
-      let bbox = bbox xmin xmax ymin ymax zmin zmax mmin mmax in
-      let length = Int32.to_int length and version = Int32.to_int version
-      and shape_type = Int32.to_int shape_type in
-      { length; version; shape_type; bbox }, contents
-  | {_} -> failwith "Shp.header"
+  | { v: 64 : littleendian, bind (b2f v); rem: -1 : bitstring } -> v, rem
 
-let record_header = fun bits ->
-  bitmatch bits with
-  | { n: 32 : bigendian; l: 32 : bigendian; payload: -1 : bitstring } ->
-      Int32.to_int n, Int32.to_int l, payload
-  | {_} -> failwith "Shp.record_header"
+let make_point = fun size bits ->
+  let bits = ref bits in
+  let p = Array.init size (fun _ ->
+    let v, rem = get_float !bits in
+    bits := rem; v) in
+  p, !bits
+
+let make_bbox = fun size -> make_point (2 * size)
 
 let make_parts = fun n bits ->
   let bparts = ref bits in
   Array.init n (fun i ->
     let idx, rest = bitmatch !bparts with
-      | { idx: 32 : littleendian; r: -1 : bitstring } -> Int32.to_int idx, r
-      | {_} -> failwith "Shp.make_parts" in
+	      | { idx: 32 : littleendian; r: -1 : bitstring } -> b2i idx, r
+	      | { _ } -> failwith "Shp.make_parts" in
     bparts := rest; idx)
 
-let make_points2D = fun n bits ->
+let make_points = fun size n bits ->
   let bpoints = ref bits in
-  Array.init n (fun i ->
-    let x, y, rest = bitmatch !bpoints with
-      | { x: 64 : littleendian; y: 64 : littleendian; r: -1 : bitstring } ->
-	  Int64.float_of_bits x, Int64.float_of_bits y, r
-      | {_} -> failwith "Shp.make_points2D" in
-    bpoints := rest; { x; y; z = 0.; m = 0. })
+  Array.init n (fun _ ->
+    let p, rest = make_point size bits in
+    bpoints := rest; p)
 
-let make_rings = fun nparts npoints parts points ->
+let make_rings = fun size nparts npoints parts points ->
   let parts = make_parts nparts parts
-  and points = make_points2D npoints points in
+  and points = make_points size npoints points in
   Array.init nparts (fun i ->
     Array.sub points parts.(i)
       ((if i < nparts - 1 then parts.(i+1) else npoints) - parts.(i)))
 
-type points = point array
+let multipoint = fun size bits ->
+  let bbox, bits = make_bbox size bits in
+  bitmatch bits with
+  | { npoints: 32 : littleendian, bind (b2i npoints);
+      points: size * 64 * npoints : bitstring;
+      rest: -1 : bitstring } ->
+	bbox, make_points size npoints points, rest
+
+let multishape = fun size bits ->
+  let bbox, bits = make_bbox size bits in
+  bitmatch bits with
+  | { nparts: 32 : littleendian, bind (b2i nparts);
+      npoints: 32 : littleendian, bind (b2i npoints);
+      parts: 32 * nparts : bitstring;
+      points: size * 64 * npoints : bitstring;
+      rest: -1 : bitstring } ->
+	let shapes = make_rings size nparts npoints parts points in
+	bbox, shapes, rest
+
+module XY = struct
+
+  let size = 2
+
+  type point = { x: float; y: float }
+
+  let print_point = fun p -> Printf.printf "x = %f, y = %f" p.x p.y
+
+  type bbox = { xmin: float; xmax: float; ymin: float; ymax: float }
+
+  let print_bbox = fun b ->
+    Printf.printf "xmin = %f, xmax = %f\n" b.xmin b.xmax;
+    Printf.printf "ymin = %f, ymax = %f\n" b.ymin b.ymax
+
+end
+
+module XYM = struct
+
+  type point = { x: float; y: float; m: float }
+
+  let print_point = fun p ->
+    Printf.printf "x = %f, y = %f, m = %f\n" p.x p.y p.m
+
+  type bbox = { xmin: float; xmax: float; ymin: float; ymax: float;
+		mmin: float; mmax: float }
+
+  let print_bbox = fun b ->
+    Printf.printf "xmin = %f, xmax = %f\n" b.xmin b.xmax;
+    Printf.printf "ymin = %f, ymax = %f\n" b.ymin b.ymax;
+    Printf.printf "mmin = %f, mmax = %f\n" b.mmin b.mmax
+
+end
+
+module XYZM = struct
+
+  type point = { x: float; y: float; z: float; m: float }
+
+  let print_point = fun p ->
+    Printf.printf "x = %f, y = %f, z = %f, m = %f\n" p.x p.y p.z p.m
+
+  type bbox = { xmin: float; xmax: float; ymin: float; ymax: float;
+		zmin: float; zmax: float; mmin: float; mmax: float }
+
+  let print_bbox = fun b ->
+    Printf.printf "xmin = %f, xmax = %f\n" b.xmin b.xmax;
+    Printf.printf "ymin = %f, ymax = %f\n" b.ymin b.ymax;
+    Printf.printf "zmin = %f, zmax = %f\n" b.zmin b.zmax;
+    Printf.printf "mmin = %f, mmax = %f\n" b.mmin b.mmax
+
+  let make_bbox = fun bits -> (* TODO : must disappear *)
+    bitmatch bits with
+    | { xmin: 64 : littleendian, bind (b2f xmin);
+	ymin: 64 : littleendian, bind (b2f ymin);
+	xmax: 64 : littleendian, bind (b2f xmax);
+	ymax: 64 : littleendian, bind (b2f ymax);
+	zmin: 64 : littleendian, bind (b2f zmin);
+	zmax: 64 : littleendian, bind (b2f zmax);
+	mmin: 64 : littleendian, bind (b2f mmin);
+	mmax: 64 : littleendian, bind (b2f mmax);
+	rest: -1 : bitstring } ->
+	  { xmin; xmax; ymin; ymax; zmin; zmax; mmin; mmax }, rest
+
+end
+
+type header = { length: int; version: int; shape_type: int; bbox: XYZM.bbox }
+
+let print_header = fun h ->
+  Printf.printf "file length = %d\n%!" h.length;
+  Printf.printf "version = %d\n%!" h.version;
+  Printf.printf "type = %d\n%!" h.shape_type;
+  XYZM.print_bbox h.bbox
+
+let header = fun bits ->
+  bitmatch bits with
+  | { code: 32 : bigendian, check (b2i code = 9994);
+      _: 5 * 32 : bitstring; (* 5 unused fields (32 bits each) *)
+      length: 32 : bigendian, bind (b2i length);
+      version: 32 : littleendian, bind (b2i version);
+      shape_type: 32 : littleendian, bind (b2i shape_type);
+      rest: -1 : bitstring } ->
+	let bbox, contents = XYZM.make_bbox rest in
+	{ length; version; shape_type; bbox }, contents
+  | { _ } -> failwith "Shp.header"
+
+let record_header = fun bits ->
+  bitmatch bits with
+  | { n: 32 : bigendian; l: 32 : bigendian; payload: -1 : bitstring } ->
+      b2i n, b2i l, payload
+  | {_} -> failwith "Shp.record_header"
 
 type shape =
   | Null
   | Point of point
-  | MultiPoint of bbox * points
-  | PolyLine of bbox * points array
-  | Polygon of bbox * points array
-  | PointZ | PolyLineZ | PolygonZ | MultiPointZ
-  | PointM | PolyLineM | PolygonM | MultiPointM | MultiPatch
+  | MultiPoint of bbox * point array
+  | PolyLine of bbox * point array array
+  | Polygon of bbox * point array array
+  | PointM of point
+  | PolyLineM of bbox * point array array
+  | PolygonM of bbox * point array array
+  | MultiPointM of bbox * point array
+  | PointZ of point
+  | PolyLineZ of bbox * point array array
+  | PolygonZ of bbox * point array array
+  | MultiPointZ of bbox * point array
+  | MultiPatch
 
 let record = fun bits ->
-  bitmatch bits with
-  | { shape: 32 : littleendian, check (Int32.to_int shape = 0); (* Null *)
-      rest: -1 : bitstring } -> Null, rest
-  | { shape: 32 : littleendian, check (Int32.to_int shape = 1); (* Point *)
-      x: 64 : littleendian; y: 64 : littleendian;
-      rest: -1 : bitstring } ->
-	let x = Int64.float_of_bits x and y = Int64.float_of_bits y in
-	Point { x; y; z = 0.; m = 0. }, rest
-(* TODO : multipoint *)
-  | { shape: 32 : littleendian, bind (Int32.to_int shape), (* PolyLine or Polygon *)
-      check (Int32.to_int shape = 3 || Int32.to_int shape = 5);
-      xmin: 64 : littleendian; ymin: 64 : littleendian;
-      xmax: 64 : littleendian; ymax: 64 : littleendian;
-      nparts: 32 : littleendian; npoints: 32 : littleendian;
-      parts: 32 * (Int32.to_int nparts) : bitstring;
-      points: 128 * (Int32.to_int npoints) : bitstring;
-      rest: -1 : bitstring
-    } ->
-      let bbox = bbox2D xmin xmax ymin ymax in
-      let nparts = Int32.to_int nparts and npoints = Int32.to_int npoints in
-      let rings = make_rings nparts npoints parts points in
-      let shp =
-	if shape = 3 then PolyLine (bbox, rings)
-	else Polygon (bbox, rings) in
-      shp, rest
+  let shape_type, bits = bitmatch bits with
+  | { shp: 32 : littleendian, bind (b2i shp); r: -1 : bitstring } -> shp, r in
+  match shape_type with
+  | 00 -> Null, bits
+  | 01 -> let p, r = make_point xy bits in Point p, r
+  | 11 -> let p, r = make_point xyzm bits in PointZ p, r
+  | 21 -> let p, r = make_point xym bits in PointM p, r
+  | 08 -> let box, pts, r = multipoint xy bits in MultiPoint (box, pts), r
+  | 18 -> let box, pts, r = multipoint xyzm bits in MultiPointZ (box, pts), r
+  | 28 -> let box, pts, r = multipoint xym bits in MultiPointM (box, pts), r
+  | 03 -> let box, shps, r = multishape xy bits in PolyLine (box, shps), r
+  | 05 -> let box, shps, r = multishape xy bits in Polygon (box, shps), r
+  | 13 -> let box, shps, r = multishape xyzm bits in PolyLineZ (box, shps), r
+  | 15 -> let box, shps, r = multishape xyzm bits in PolygonZ (box, shps), r
+  | 23 -> let box, shps, r = multishape xym bits in PolyLineM (box, shps), r
+  | 25 -> let box, shps, r = multishape xym bits in PolygonM (box, shps), r
+  | 31 -> failwith "Not implemented"
+  | _ -> failwith "Not a documented shape"
 
 let records = fun bits ->
   let res = ref [] and bits = ref bits in
--- shp.mli
+++ shp.mli
@@ -1,16 +1,55 @@
-type point = { x: float; y: float; z: float; m: float }
-type bbox = { xmin: float; xmax: float; ymin: float; ymax: float;
-	      zmin: float; zmax: float; mmin: float; mmax: float }
+module XY : sig
+  type point = { x: float; y: float }
+  type bbox = { xmin: float; xmax: float; ymin: float; ymax: float }
+end
 
-type points = point array
+module XYM : sig
+  type point = { x: float; y: float; m: float }
+  type bbox = { xmin: float; xmax: float; ymin: float; ymax: float;
+		mmin: float; mmax: float }
+end
+
+module XYZM : sig
+  type point = { x: float; y: float; z: float; m: float }
+  type bbox = { xmin: float; xmax: float; ymin: float; ymax: float;
+		zmin: float; zmax: float; mmin: float; mmax: float }
+end
+
+type point = float array
+type bbox = float array
+
+(*
+type shape =
+  | Null
+  | Point of XY.point
+  | MultiPoint of XY.bbox * XY.point array
+  | PolyLine of XY.bbox * XY.point array array
+  | Polygon of XY.bbox * XY.point array array
+  | PointM of XYM.point
+  | PolyLineM of XYM.bbox * XYM.point array array
+  | PolygonM of XYM.bbox * XYM.point array array
+  | MultiPointM of XYM.bbox * XYM.point array
+  | PointZ of XYZM.point
+  | PolyLineZ of XYZM.bbox * XYZM.point array array
+  | PolygonZ of XYZM.bbox * XYZM.point array array
+  | MultiPointZ of XYZM.bbox * XYZM.point array
+  | MultiPatch
+*)
 
 type shape =
   | Null
   | Point of point
-  | MultiPoint of bbox * points
-  | PolyLine of bbox * points array
-  | Polygon of bbox * points array
-  | PointZ | PolyLineZ | PolygonZ | MultiPointZ
-  | PointM | PolyLineM | PolygonM | MultiPointM | MultiPatch
+  | MultiPoint of bbox * point array
+  | PolyLine of bbox * point array array
+  | Polygon of bbox * point array array
+  | PointM of point
+  | PolyLineM of bbox * point array array
+  | PolygonM of bbox * point array array
+  | MultiPointM of bbox * point array
+  | PointZ of point
+  | PolyLineZ of bbox * point array array
+  | PolygonZ of bbox * point array array
+  | MultiPointZ of bbox * point array
+  | MultiPatch
 
 val read: string -> shape list
