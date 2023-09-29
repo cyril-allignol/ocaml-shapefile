ec9142933907e68708731b9db393157530155d37
zapashcanon <leo@ndrs.fr>
2023-09-29 22:01:39 +0200

    rewrite printing to use a fmt instead of producing strings

--- src/prj_syntax.ml
+++ src/prj_syntax.ml
@@ -12,16 +12,16 @@
  * License for the specific language governing permissions and limitations
  * under the License. *)
 
-let sprint_opt sprint = function
-  | None -> ""
-  | Some e -> Printf.sprintf ", %s" (sprint e)
+let pp_opt pp_v fmt o =
+  Format.pp_print_option (fun fmt v -> Format.fprintf fmt ", %a" pp_v v) fmt o
 
-let sprint_list f l =
-  List.map f l |> String.concat ", "
+let pp_list pp_v fmt l =
+  Format.pp_print_list ~pp_sep:(fun fmt () -> Format.fprintf fmt ", ") pp_v fmt l
 
 module Parameter = struct
   type t = { name: string; value: float }
-  let sprint = fun p -> Printf.sprintf "PARAMETER[\"%s\", %f]" p.name p.value
+  let pp fmt { name; value } =
+    Format.fprintf fmt {|PARAMETER["%s", %f]"|} name value
 end
 
 module MT = struct
@@ -31,32 +31,37 @@ module MT = struct
     | Inverse of t
     | Passthrough of int * t
 
-  let rec sprint = function
+  let rec pp fmt = function
     | Param (name, ps) ->
-	Printf.sprintf "PARAM_MT[\"%s\"%s]" name
-	  (sprint_list Parameter.sprint ps)
+	Format.fprintf fmt {|PARAM_MT["%s"%a]|} name (pp_list Parameter.pp) ps
     | Concat (mt :: mts) ->
-	Printf.sprintf "CONCAT_MT[%s%s]" (sprint mt) (sprint_list sprint mts)
+	Format.fprintf fmt "CONCAT_MT[%a%a]" pp mt (pp_list pp) mts
     | Concat _ -> invalid_arg "Prj_syntax.MT"
-    | Inverse mt -> Printf.sprintf "INVERSE_MT[%s]" (sprint mt)
+    | Inverse mt -> Format.fprintf fmt "INVERSE_MT[%a]" pp mt
     | Passthrough (i, mt) ->
-	Printf.sprintf "PASSTHROUGH_MT[%d, %s]" i (sprint mt)
+	Format.fprintf fmt "PASSTHROUGH_MT[%d, %a]" i pp mt
 
 end
 
 module Authority = struct
   type t = { name: string; code: string }
-  let sprint = fun a -> Printf.sprintf "AUTHORITY[\"%s\", \"%s\"]" a.name a.code
+  let pp fmt { name; code } =
+    Format.fprintf fmt {|AUTHORITY["%s", "%s"]|} name code
 end
 
 module Axis = struct
   type direction = North | South | East | West | Up | Down | Other
   type t = { name: string; direction: direction }
-  let sprint_dir = function
-    | North -> "NORTH" | South -> "SOUTH" | East -> "EAST" | West -> "WEST"
-    | Up -> "UP" | Down -> "DOWN" | Other -> "OTHER"
-  let sprint = fun a ->
-    Printf.sprintf "AXIS[\"%s\", %s]" a.name (sprint_dir a.direction)
+  let pp_dir fmt = function
+    | North -> Format.pp_print_string fmt "NORTH"
+    | South -> Format.pp_print_string fmt "SOUTH"
+    | East -> Format.pp_print_string fmt "EAST"
+    | West -> Format.pp_print_string fmt "WEST"
+    | Up -> Format.pp_print_string fmt "UP"
+    | Down -> Format.pp_print_string fmt "DOWN"
+    | Other -> Format.pp_print_string fmt "OTHER"
+  let pp fmt { name; direction } =
+  Format.fprintf fmt {|AXIS["%s", %a]|} name pp_dir direction
 
   let geographic_default =
     { name = "Lon"; direction = East }, { name = "Lat"; direction = North }
@@ -69,119 +74,119 @@ end
 
 module Unit = struct
   type t = { name: string; cf: float; authority: Authority.t option }
-  let sprint = fun u ->
-    Printf.sprintf "UNIT[\"%s\", %f%s]" u.name u.cf
-      (sprint_opt Authority.sprint u.authority)
+  let pp fmt { name; cf; authority } =
+    Format.fprintf fmt {|UNIT["%s", %f%a]|} name cf
+      (pp_opt Authority.pp) authority
 end
 
 module Primem = struct
   type t = { name: string; longitude: float; authority: Authority.t option }
-  let sprint = fun pm ->
-    Printf.sprintf "PRIMEM[\"%s\", %f%s]" pm.name pm.longitude
-      (sprint_opt Authority.sprint pm.authority)
+  let pp fmt { name; longitude; authority } =
+    Format.fprintf fmt {|PRIMEM["%s", %f%a]|} name longitude
+      (pp_opt Authority.pp) authority
 end
 
 module ToWGS84 = struct
   type t = { dx: float; dy: float; dz: float;
 	     ex: float; ey: float; ez: float;
 	     ppm: float }
-  let sprint = fun t ->
-    Printf.sprintf "TOWGS84[%f, %f, %f, %f, %f, %f, %f]"
-      t.dx t.dy t.dz t.ex t.ey t.ez t.ppm
+  let pp fmt { dx; dy; dz; ex; ey; ez; ppm } =
+    Format.fprintf fmt "TOWGS84[%f, %f, %f, %f, %f, %f, %f]"
+      dx dy dz ex ey ez ppm
 end
 
 module Spheroid = struct
   type t = { name: string; a: float; f: float; authority: Authority.t option }
-  let sprint = fun s ->
-    Printf.sprintf "SPHEROID[\"%s\", %f, %f%s]"
-      s.name s.a s.f (sprint_opt Authority.sprint s.authority)
+  let pp fmt { name; a; f; authority } =
+    Format.fprintf fmt {|SPHEROID["%s", %f, %f%a]|}
+      name a f (pp_opt Authority.pp) authority
 end
 
 module Datum = struct
   type t = { name: string; spheroid: Spheroid.t;
 	     toWGS84: ToWGS84.t option; authority: Authority.t option }
-  let sprint = fun s ->
-    Printf.sprintf "DATUM[\"%s\", %s%s%s]" s.name (Spheroid.sprint s.spheroid)
-      (sprint_opt ToWGS84.sprint s.toWGS84)
-      (sprint_opt Authority.sprint s.authority)
+  let pp fmt { name; spheroid; toWGS84; authority } =
+    Format.fprintf fmt {|DATUM["%s", %a%a%a]|} name Spheroid.pp spheroid
+      (pp_opt ToWGS84.pp) toWGS84
+      (pp_opt Authority.pp) authority
 end
 
 module Vert_datum = struct
   type t = { name: string; datum_type: float; authority: Authority.t option }
-  let sprint = fun vd ->
-    Printf.sprintf "VERT_DATUM[\"%s\", %f%s]"
-      vd.name vd.datum_type (sprint_opt Authority.sprint vd.authority)
+  let pp fmt { name; datum_type; authority } =
+    Format.fprintf fmt {|VERT_DATUM["%s", %f%a]|}
+      name datum_type (pp_opt Authority.pp) authority
 end
 
 module Local_datum = struct
   type t = { name: string; datum_type: float; authority: Authority.t option }
-  let sprint = fun ld ->
-    Printf.sprintf "LOCAL_DATUM[\"%s\", %f%s]"
-      ld.name ld.datum_type (sprint_opt Authority.sprint ld.authority)
+  let pp fmt { name; datum_type; authority } =
+    Format.fprintf fmt {|LOCAL_DATUM["%s", %f%a]|}
+      name datum_type (pp_opt Authority.pp) authority
 end
 
 module Projection = struct
   type t = { name: string; authority: Authority.t option }
-  let sprint = fun p ->
-    Printf.sprintf "PROJECTION[\"%s\"%s]"
-      p.name (sprint_opt Authority.sprint p.authority)
+  let pp fmt { name; authority } =
+    Format.fprintf fmt {|PROJECTION["%s"%a]|}
+      name (pp_opt Authority.pp) authority
 end
 
 module GeogCS = struct
   type t = { name: string; datum: Datum.t; prime_meridian: Primem.t;
 	     angular_unit: Unit.t; axes: Axis.t * Axis.t;
 	     authority: Authority.t option }
-  let sprint = fun g ->
-    let lon_axis, lat_axis = g.axes in
-    Printf.sprintf "GEOGCS[\"%s\", %s, %s, %s, %s, %s%s]"
-      g.name (Datum.sprint g.datum) (Primem.sprint g.prime_meridian)
-      (Unit.sprint g.angular_unit) (Axis.sprint lon_axis) (Axis.sprint lat_axis)
-      (sprint_opt Authority.sprint g.authority)
+  let pp fmt { axes; name; prime_meridian; angular_unit; authority; datum } =
+    let lon_axis, lat_axis = axes in
+    Format.fprintf fmt {|GEOGCS["%s", %a, %a, %a, %a, %a%a]|}
+      name Datum.pp datum Primem.pp prime_meridian
+      Unit.pp angular_unit Axis.pp lon_axis Axis.pp lat_axis
+      (pp_opt Authority.pp) authority
 end
 
 module ProjCS = struct
   type t = { name: string; geogcs: GeogCS.t; projection: Projection.t;
 	     params: Parameter.t list; linear_unit: Unit.t;
 	     axes: Axis.t * Axis.t; authority: Authority.t option }
-  let sprint = fun p ->
-    let x_axis, y_axis = p.axes in
-    Printf.sprintf "PROJCS[\"%s\", %s, %s, %s, %s, %s, %s%s]"
-      p.name (GeogCS.sprint p.geogcs) (Projection.sprint p.projection)
-      (sprint_list Parameter.sprint p.params) (Unit.sprint p.linear_unit)
-      (Axis.sprint x_axis) (Axis.sprint y_axis)
-      (sprint_opt Authority.sprint p.authority)
+  let pp fmt { axes; name; geogcs; projection; params; linear_unit; authority } =
+    let x_axis, y_axis = axes in
+    Format.fprintf fmt {|PROJCS["%s", %a, %a, %a, %a, %a, %a%a]|}
+      name GeogCS.pp geogcs Projection.pp projection
+      (pp_list Parameter.pp) params Unit.pp linear_unit
+      Axis.pp x_axis Axis.pp y_axis
+      (pp_opt Authority.pp) authority
 end
 
 module GeocCS = struct
   type t = { name: string; datum: Datum.t; prime_meridian: Primem.t;
 	     linear_unit: Unit.t; axes: Axis.t * Axis.t * Axis.t;
 	     authority: Authority.t option }
-  let sprint = fun g ->
-    let x_axis, y_axis, z_axis = g.axes in
-    Printf.sprintf "GEOCCS[\"%s\", %s, %s, %s, %s, %s, %s%s]"
-      g.name (Datum.sprint g.datum) (Primem.sprint g.prime_meridian)
-      (Unit.sprint g.linear_unit)
-      (Axis.sprint x_axis) (Axis.sprint y_axis) (Axis.sprint z_axis)
-      (sprint_opt Authority.sprint g.authority)
+  let pp fmt { name; datum; prime_meridian; linear_unit; axes; authority } =
+    let x_axis, y_axis, z_axis = axes in
+    Format.fprintf fmt {|GEOCCS["%s", %a, %a, %a, %a, %a, %a%a]|}
+      name Datum.pp datum Primem.pp prime_meridian
+      Unit.pp linear_unit
+      Axis.pp x_axis Axis.pp y_axis Axis.pp z_axis
+      (pp_opt Authority.pp) authority
 end
 
 module VertCS = struct
   type t = { name: string; datum: Vert_datum.t; linear_unit: Unit.t;
 	     axis: Axis.t; authority: Authority.t option }
-  let sprint = fun vd ->
-    Printf.sprintf "VERT_CS[\"%s\", %s, %s, %s%s]"
-      vd.name (Vert_datum.sprint vd.datum) (Unit.sprint vd.linear_unit)
-      (Axis.sprint vd.axis) (sprint_opt Authority.sprint vd.authority)
+  let pp fmt { name; datum; linear_unit; authority; axis } =
+    Format.fprintf fmt {|VERT_CS["%s", %a, %a, %a%a]|}
+      name Vert_datum.pp datum Unit.pp linear_unit
+      Axis.pp axis (pp_opt Authority.pp) authority
 end
 
 module LocalCS = struct
   type t = { name: string; datum: Local_datum.t; unit: Unit.t;
 	     axes: Axis.t list; authority: Authority.t option }
-  let sprint = fun ld ->
-    Printf.sprintf "LOCAL_CS[\"%s\", %s, %s, %s%s]"
-      ld.name (Local_datum.sprint ld.datum) (Unit.sprint ld.unit)
-      (sprint_list Axis.sprint ld.axes)
-      (sprint_opt Authority.sprint ld.authority)
+  let pp fmt { name; datum; unit; axes; authority } =
+    Format.fprintf fmt {|LOCAL_CS["%s", %a, %a, %a%a]|}
+      name Local_datum.pp datum Unit.pp unit
+      (pp_list Axis.pp) axes
+      (pp_opt Authority.pp) authority
 end
 
 module CS = struct
@@ -194,17 +199,17 @@ module CS = struct
     | Fitted of string * MT.t * t
     | Local of LocalCS.t
 
-  let rec sprint = function
-    | Geographic cs -> GeogCS.sprint cs
-    | Projected cs -> ProjCS.sprint cs
-    | Geocentric cs -> GeocCS.sprint cs
-    | Vert cs -> VertCS.sprint cs
+  let rec pp fmt = function
+    | Geographic cs -> GeogCS.pp fmt cs
+    | Projected cs -> ProjCS.pp fmt cs
+    | Geocentric cs -> GeocCS.pp fmt cs
+    | Vert cs -> VertCS.pp fmt cs
     | Compd (name, head_cs, tail_cs, authority) ->
-	Printf.sprintf "COMPD_CS[\"%s\", %s, %s%s]"
-	  name (sprint head_cs) (sprint tail_cs)
-	  (sprint_opt Authority.sprint authority)
+	Format.fprintf fmt {|COMPD_CS["%s", %a, %a%a]|}
+	  name pp head_cs pp tail_cs
+	  (pp_opt Authority.pp) authority
     | Fitted (name, to_base, base_cs) ->
-	Printf.sprintf "FITTED_CS[\"%s\", %s, %s]"
-	  name (MT.sprint to_base) (sprint base_cs)
-    | Local cs -> LocalCS.sprint cs
+	Format.fprintf fmt {|FITTED_CS["%s", %a, %a]|}
+	  name MT.pp to_base pp base_cs
+    | Local cs -> LocalCS.pp fmt cs
 end
